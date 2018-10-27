4528ae23dc9e316c88645b25e059a6591aa165bc
marialobillo <maria.lobillo.santos@gmail.com>
2018-10-27 14:05:59 +0200

    setup and connecting to db

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://maria:maria11@ds033966.mlab.com:33966/devconnector'
+}
--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello World'));
 
