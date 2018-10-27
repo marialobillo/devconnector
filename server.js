49ee5963d95baf2449cd7f7172bbd76ce181f505
marialobillo <maria.lobillo.santos@gmail.com>
2018-10-27 17:22:22 +0200

    using routes

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const express = require('express');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+
 const app = express();
 const mongoose = require('mongoose');
 
@@ -12,6 +18,11 @@ mongoose
   .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('Hello World'));
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
