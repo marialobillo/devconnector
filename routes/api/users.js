8533ee01987d68972ffbd5a5c6c962fa57699ad2
marialobillo <maria.lobillo.santos@gmail.com>
2018-10-30 16:35:16 +0100

    login validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -66,6 +66,12 @@ router.post('/register', (req, res) => {
 // @desc Login user /Returning JWT Token
 // @access Public
 router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  // Check Validation
+  if(!isValid){
+    return res.status(400).json(errors);
+  }
   const email = req.body.email;
   const password = req.body.password;
 
@@ -74,7 +80,8 @@ router.post('/login', (req, res) => {
     .then(user => {
       // Check for user
       if(!user){
-        return res.status(404).json({email: 'User email not found'});
+        errors.email = 'User not found';
+        return res.status(404).json(errors);
       }
       // Check Password
       bcrypt.compare(password, user.password)
@@ -95,7 +102,8 @@ router.post('/login', (req, res) => {
                 });
             });
           } else {
-            return res.status(400).json({ password: 'Password incorrect'});
+            errors.password = 'Password incorrect';
+            return res.status(400).json(errors);
           }
         })
     })
--- validation/login.js
+++ validation/login.js
@@ -10,22 +10,19 @@ module.exports = function validateLoginInput(data){
   data.password = !isEmpty(data.password) ? data.password : '';
 
 
+
+  if(!Validator.isEmail(data.email)){
+    errors.email = 'Email field is invalid';
+  }
+
   if(Validator.isEmpty(data.email)){
     errors.email = 'Email field is required';
   }
 
-  if(Validator.isEmail(data.email)){
-    errors.email = 'Email field is invalid';
-  }
 
   if(Validator.isEmpty(data.password)){
     errors.password = 'Password field is required';
   }
-
-  if(!Validator.isLength(data.password), {min: 6, max: 30}){
-    errors.passowrd = 'Password must be at least 6 characters';
-  }
-
 
   return {
     errors: errors,
